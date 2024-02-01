163b140b249fbb0c8f1b3dc2cf8ee9f705297ef2
Thompson Silva <thompson.msds@gmail.com>
2024-02-01 15:58:05 -0300

    add: routers client

--- api/src/infrastructure/http/controllers/ClientController.ts
+++ api/src/infrastructure/http/controllers/ClientController.ts
@@ -4,4 +4,20 @@ export class ClientController {
     store(req: Request, res: Response): Response<any> {
         return res.status(201).json({ message: 'teste' })
     }
+
+    update(req: Request, res: Response): Response<any> {
+        return res.status(201).json({ message: 'teste' })
+    }
+
+    destroy(req: Request, res: Response): Response<any> {
+        return res.status(201).json({ message: 'teste' })
+    }
+
+    index(req: Request, res: Response): Response<any> {
+        return res.status(201).json({ message: 'teste' })
+    }
+
+    show(req: Request, res: Response): Response<any> {
+        return res.status(201).json({ message: 'teste' })
+    }
 }
--- api/src/routers/index.ts
+++ api/src/routers/index.ts
@@ -14,10 +14,13 @@ export class Routers {
     }
 
     private routesClient(): void {
+        const baseURL = '/client'
         const controller = new ClientController()
 
-        this.router.post('/create-client', (req, res) =>
-            controller.store(req, res)
-        )
+        this.router.post(`${baseURL}`, (req, res) => controller.store(req, res))
+        this.router.put(`${baseURL}/:id`, (req, res) => controller.update(req, res))
+        this.router.delete(`${baseURL}/:id`, (req, res) => controller.destroy(req, res))
+        this.router.get(`${baseURL}`, (req, res) => controller.index(req, res))
+        this.router.get(`${baseURL}/:id`, (req, res) => controller.show(req, res))
     }
 }
